ad9049baae56b2c1b1e819573cbe233e2d2f1af5
Rob Wilson <rwilson127@student.umgc.edu>
2021-06-15 21:19:09 -0700

    Add BranchList component

---
+++ Blazor_Server/Data/GitHubService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Octokit;
+
+namespace Blazor_Server.Data
+{
+    public class GitHubService
+    {
+        private const string Org = "umgc";
+
+        private GitHubClient client = new GitHubClient(new ProductHeaderValue("UMGC", "0.1"));
+
+        public async Task<List<Repository>> GetRepositories()
+        {
+            // Get repositories for the org
+            var repositories = await client.Repository.GetAllForOrg(Org);
+
+            List<Repository> repos = new List<Repository>();
+
+            // Sort by updated and skip any that are archived.
+            foreach(var repo in repositories.OrderByDescending(r => r.UpdatedAt))
+            {
+                if (repo.Archived)
+                {
+                    continue;
+                }
+
+                repos.Add(repo);
+            }
+
+            return repos;
+        }
+    }
+}
--- Blazor_Server/Startup.cs
+++ Blazor_Server/Startup.cs
@@ -47,6 +47,7 @@ namespace Blazor_Server
             services.AddServerSideBlazor()
                 .AddMicrosoftIdentityConsentHandler();
             services.AddSingleton<WeatherForecastService>();
+            services.AddSingleton<GitHubService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
