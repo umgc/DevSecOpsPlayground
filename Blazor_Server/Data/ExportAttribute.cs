fcc778b01a9382ff2b93291e88027a9e6826c1d3
Rob Wilson <rwilson127@student.umgc.edu>
2021-06-25 21:23:25 -0700

    Create attribute to prevent items from export

---
+++ Blazor_Server/Data/ExportAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Blazor_Server.Data
+{
+    public class ExportAttribute : Attribute
+    {
+        public ExportAttribute(bool canExport)
+        {
+            this.CanExport = canExport;
+        }
+
+        public bool CanExport { get; private set; } = false;
+    }
+}
