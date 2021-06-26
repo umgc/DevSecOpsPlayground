ce360fef582137c0395568c39a18207a269403d9
Rob Wilson <rwilson127@student.umgc.edu>
2021-06-25 21:21:17 -0700

    Add Comment Class to store comments

---
+++ Blazor_Server/Data/Comment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Blazor_Server.Data
+{
+    public class Comment
+    {
+        public Comment(Guid projectId)
+        {
+            this.ProjectID = projectId;
+            this.CommentId = Guid.NewGuid();
+        }
+
+        public Guid CommentId { get; set; }
+
+        public string Comments { get; set; }
+
+        public string UserEmail { get; set; }
+
+        public DateTime UpdateDateTime { get; set; }
+
+        public Guid ProjectID { get; private set; }
+    }
+}
