71c99f59e1c6980b0a3562b12b46d7de72d3b5fe
Rob Wilson <rwilson127@student.umgc.edu>
2021-06-27 10:29:12 -0700

    Generalize the table add provisions for filtering

---
+++ Blazor_Server/Shared/TableDataChangedEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Blazor_Server.Shared
+{
+    public class TableDataChangedEventArgs : EventArgs
+    {
+        public TableDataChangedEventArgs(IEnumerable<object> data)
+        {
+            this.Data = data;
+        }
+
+        public IEnumerable<object> Data { get; private set; }
+    }
+}
