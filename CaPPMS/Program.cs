58dc3f048db4c6b2e4cb7f56def254cc621ba213
Rob Wilson <rwilson127@student.umgc.edu>
2021-07-11 10:36:15 -0700

    Allow Host properties to be used througout application

--- CaPPMS/Program.cs
+++ CaPPMS/Program.cs
@@ -1,26 +1,29 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CaPPMS
 {
     public class Program
     {
+        public static IDictionary<object, object> HostProperties { get; private set; }
+
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var host = Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+            HostProperties = host.Properties;
+
+            return host;
+        }
     }
 }
