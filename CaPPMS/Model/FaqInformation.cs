41f0554995d2bae712ae6810e91c9b92f1d0425e
michellemonfort <mmonfort2@student.umgc.edu>
2021-07-21 22:39:37 -0400

    update faq model

--- CaPPMS/Model/FaqInformation.cs
+++ CaPPMS/Model/FaqInformation.cs
@@ -6,6 +6,14 @@ namespace CaPPMS.Model
     {
         public Guid Guid { get; } = Guid.NewGuid();
 
+        public string Question { get; set; } = string.Empty;
+
+        public string Author { get; set; } = string.Empty;
+
+        public string Reply { get; set; } = string.Empty;
+
+        public string Topic { get; set; } = string.Empty;
+
     }
 
 }
