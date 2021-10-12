4cc961e9e27f087961706af1cde1c77b3bb680c9
addisu292 <75600776+addisu292@users.noreply.github.com>
2021-10-12 16:59:24 -0400

    adding a few more test cases (#117)
    Co-authored-by: Addisu Worku <addget33@gmail.com>

--- CaPPMSTests/Model/ContactTests.cs
+++ CaPPMSTests/Model/ContactTests.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CaPPMS.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace CaPPMSTests.Model
 {
-    class ContactTests
+    [TestClass]
+    public class ContactTests
     {
+        [TestMethod]
+        public void ContactInitialization()
+        {
+            var contact = new Contact();
+
+            foreach(var prop in contact.GetType().GetProperties())
+            {
+                if(prop.PropertyType != typeof(Guid))
+                {
+                    Assert.IsNotNull(prop.GetValue(contact));
+                }
+                else if (prop.PropertyType == typeof(Guid))
+                {
+                    Assert.IsFalse((Guid)prop.GetValue(contact) == Guid.Empty);
+                }
+            }
+        }
     }
 }
--- CaPPMSTests/Model/ProjectInformationTests.cs
+++ CaPPMSTests/Model/ProjectInformationTests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CaPPMS.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,54 @@ using System.Threading.Tasks;
 
 namespace CaPPMSTests.Model
 {
-    class ProjectInformationTests
+    /// <summary>
+    /// Test the ProjectInformationTests class functionality
+    /// </summary>
+    [TestClass]
+    public class ProjectInformationTests
     {
+        /// <summary>
+        /// Test fields initialization
+        /// </summary>
+        [TestMethod]
+        public void TestInitialization()
+        {
+            ProjectInformation faqInformation = new ProjectInformation();
+
+            foreach (var property in faqInformation.GetType().GetProperties())
+            {
+                if (property.PropertyType == typeof(Guid))
+                {
+                    Assert.AreNotEqual(property.GetValue(faqInformation), Guid.Empty);
+                }
+                else
+                {
+                    Assert.IsNotNull(property.GetValue(faqInformation));
+                }
+            }
+        }
+
+        /// <summary>
+        /// test the AddAttachments method and Attachments list
+        /// </summary>
+        [TestMethod]
+        public void TestAttachments()
+        {
+            ProjectInformation projectInformation = new ProjectInformation();
+
+            List<IProjectFile> projectFiles = new List<IProjectFile>() { new ProjectFile(), new ProjectFile() };
+
+            Assert.AreEqual(0, projectInformation.Attachments.Count);
+
+            projectInformation.AddAttachments(projectFiles);
+            Assert.AreEqual(projectFiles.Count, projectInformation.Attachments.Count);
+
+            projectInformation.AddAttachments(new List<IProjectFile>() { new ProjectFile() });
+
+            Assert.AreEqual(projectFiles.Count + 1, projectInformation.Attachments.Count);
+
+            projectInformation.SetAttachments(projectFiles);
+            Assert.AreEqual(projectFiles.Count, projectInformation.Attachments.Count);
+        }
     }
 }
