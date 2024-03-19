98b9c23671f8b61e11ac25a27ef5120d79658e0d
Khoger Dosky <kd@khogers-mbp.lan>
2024-03-19 13:58:26 -0400

    modified Projects and Manage Projects pages

--- CaPPMS/Model/ProjectInformation.cs
+++ CaPPMS/Model/ProjectInformation.cs
@@ -16,23 +16,23 @@ namespace CaPPMS.Model
 
         private string projectTitle = string.Empty;
 
-        private string teamMembers = string.Empty;
+        // private string teamMembers = string.Empty;
 
-        [Export(true)]
-        [DisplayName("Team Members")]
-        [Browsable(true)]
-        public string TeamMembers
-        {
-            get
-            {
-                return this.teamMembers;
-            }
-            set
-            {
-                this.teamMembers = value;
-                this.IsDirty = true;
-            }
-        }
+        // [Export(true)]
+        // [DisplayName("Team Members")]
+        // [Browsable(true)]
+        // public string TeamMember
+        // {
+        //     get
+        //     {
+        //         return this.teamMembers;
+        //     }
+        //     set
+        //     {
+        //         this.teamMembers = value;
+        //         this.IsDirty = true;
+        //     }
+        // }
 
         private string videoLink = string.Empty;
 
