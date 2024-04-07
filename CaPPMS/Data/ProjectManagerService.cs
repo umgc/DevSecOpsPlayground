df4a1c0736c912fcd5e0a063f222bed93d6f6043
Rob Wilson <rwilson127@student.umgc.edu>
2024-04-06 21:55:12 -0700

    remove commented code.

--- CaPPMS/Data/ProjectManagerService.cs
+++ CaPPMS/Data/ProjectManagerService.cs
@@ -219,7 +219,6 @@ namespace CaPPMS.Data
                     propertyTitleChunk.SetUnderline(2f, -2f);
                     var itemTitle = new Paragraph(propertyTitleChunk);
                     itemTitle.SpacingAfter = 1f;
-                    // itemTitle.Add(Environment.NewLine);
                     var item = new Phrase($"\t\t{exportedField.Item2}", bodyFont);
                     itemTitle.Add(item);
 
