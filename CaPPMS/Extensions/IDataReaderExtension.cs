d2d97e02b9b5328b17aefc3c057220369ea9a569
Robert Wilson <robert.wilson2530@yahoo.com>
2024-12-24 08:06:15 -0800

    Add utility extensions

---
+++ CaPPMS/Extensions/IDataReaderExtension.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data;
+using System.Reflection;
+
+namespace CaPPMS.Extensions
+{
+    public static class IDataReaderExtension
+    {
+        public static T ConvertRecord<T>(this IDataReader dataReader) where T : new()
+        {
+            ArgumentNullException.ThrowIfNull(dataReader);
+
+            T record = new();
+            typeof(T).GetProperties(BindingFlags.Instance | BindingFlags.Public)
+                .Foreach(property =>
+                {
+                    object? value = dataReader[property.Name];
+                    if (value != DBNull.Value)
+                    {
+                        property.SetValue(record, ConvertValue(value, property.PropertyType));
+                    }
+                });
+
+            return record;
+        }
+
+        private static object ConvertValue(object value, Type expectedType)
+        {
+            // Trying to auto convert to DateTime? fails.
+            if (value.TryConvertDateTime(out DateTime? result))
+            {
+                if (result == null)
+                {
+                    return DateTime.MinValue;
+                }
+
+                return result;
+            }
+
+            return value;
+        }
+    }
+}
---
+++ CaPPMS/Extensions/UtilityExtensions.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace CaPPMS.Extensions
+{
+    public static class UtilityExtensions
+    {
+        /// <summary>
+        /// Check if the object matches the filter.
+        /// </summary>
+        /// <param name="item"></param>
+        /// <param name="filter"></param>
+        /// <returns></returns>
+        public static bool IsMatch(this object item, string filter)
+        {
+            if (item == null)
+            {
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(filter))
+            {
+                return true;
+            }
+
+            PropertyInfo[] properties = item.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public);
+
+            string[] filterParts = filter.Split(new char[] {' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+            bool propertyMatch = filterParts.All(part =>
+            {
+                return properties.Any(property =>
+                {
+                    object? value = property.GetValue(item);
+                    if (value == null)
+                    {
+                        return true;
+                    }
+
+                    if (value is IEnumerable enemerable)
+                    {
+                        return string.Join(" ", enemerable).IndexOf(part, StringComparison.OrdinalIgnoreCase) >= 0;
+                    }
+
+                    return value.NullSafeToString().IndexOf(part, StringComparison.OrdinalIgnoreCase) >= 0;
+                });
+            });
+
+            return propertyMatch;
+        }
+
+        public static IEnumerable<T> Foreach<T>(this IEnumerable<T> value, Action<T> action)
+        {
+            if (value == null)
+            {
+                return [];
+            }
+
+            foreach (T item in value)
+            {
+                action?.Invoke(item);
+            }
+
+            return value;
+        }
+
+        /// <summary>
+        /// Return null safe object.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static string NullSafeToString(this object? obj)
+        {
+            return obj?.ToString() ?? "(null)";
+        }
+
+        public static bool TryConvertDateTime(this object? date, out DateTime? result)
+        {
+            result = null;
+            if (date == null)
+            {
+                return false;
+            }
+
+            if (DateTime.TryParse(date.NullSafeToString(), out DateTime parsedDate))
+            {
+                result = parsedDate;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
