9471489c3567d8880e32e62d95126df58c07c21e
Robert Wilson <robert.wilson2530@yahoo.com>
2025-03-19 20:11:06 -0700

    add a new class for more meaninful StringLength attribute

---
+++ CaPPMS/Attributes/CappmsStringLengthAttribute.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CaPPMS.Attributes
+{
+    /// <summary>
+    /// Provides a custom string length attribute that includes the current length of the string in the error message.
+    /// </summary>
+    public class CappmsStringLengthAttribute : StringLengthAttribute
+    {
+        private string? context;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CappmsStringLengthAttribute"/> class.
+        /// </summary>
+        /// <param name="maximumLength">The maximum length of the object shall be.</param>
+        public CappmsStringLengthAttribute(int maximumLength) : base(maximumLength)
+        {
+        }
+
+        /// <summary>
+        /// Override the default IsValid method to store the current string value.
+        /// </summary>
+        /// <param name="value">The object to validate.</param>
+        /// <returns><c>true</c> if valid.</returns>
+        public override bool IsValid(object? value)
+        {
+            if (value is string str)
+            {
+                this.context = str;
+            }
+
+            return base.IsValid(value);
+        }
+
+        /// <summary>
+        /// Override the default error message to include the current length of the string.
+        /// </summary>
+        /// <param name="name">Property Name.</param>
+        /// <returns>A formatted message.</returns>
+        public override string FormatErrorMessage(string name)
+        {
+            if (this.context != null)
+            {
+                string message = base.FormatErrorMessage(name);
+                message += $" Current length:{this.context.Length}.";
+                return message;
+            }
+
+            return base.FormatErrorMessage(name);
+        }
+    }
+}
---
+++ CaPPMSTests/Attribute/CappmsStringLengthAttributeTests.cs
@@ -0,0 +1,55 @@
+﻿using CaPPMS.Attributes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace CaPPMSTests.Attribute
+{
+    [TestClass]
+    public class CappmsStringLengthAttributeTests
+    {
+        [TestMethod]
+        public async Task StringIsValid()
+        {
+            // Arrange
+            var attribute = new CappmsStringLengthAttribute(100)
+            {
+                MinimumLength = 5,
+            };
+
+            string value = "This is a test string.";
+            Assert.IsTrue(attribute.IsValid(value));
+            await Task.CompletedTask;
+        }
+
+        [TestMethod]
+        public async Task StringIsInvalidMinLen()
+        {
+            // Arrange
+            var attribute = new CappmsStringLengthAttribute(100)
+            {
+                MinimumLength = 5,
+            };
+            string value = "Test";
+            Assert.IsFalse(attribute.IsValid(value));
+            string errorMessage = attribute.FormatErrorMessage("Test");
+            Assert.AreEqual("The field Test must be a string with a minimum length of 5 and a maximum length of 100. Current length:4.", errorMessage);
+
+            await Task.CompletedTask;
+        }
+
+        [TestMethod]
+        public async Task StringIsInvalidMaxLen()
+        {
+            // Arrange
+            var attribute = new CappmsStringLengthAttribute(10)
+            {
+                MinimumLength = 5,
+            };
+            string value = "This is a test string.";
+            Assert.IsFalse(attribute.IsValid(value));
+            string errorMessage = attribute.FormatErrorMessage("Test");
+            Assert.AreEqual("The field Test must be a string with a minimum length of 5 and a maximum length of 10. Current length:22.", errorMessage);
+            await Task.CompletedTask;
+        }
+    }
+}
